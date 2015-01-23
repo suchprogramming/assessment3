738ba782516101049cb51f1913bc19555ac985a9
Tyler Whitsett <whitsett.tyler@gmail.com>
2015-01-23 12:37:17 -0800

    add basic sinatra files and routes.

--- app.rb
+++ app.rb
@@ -6,3 +6,30 @@ require("./lib/clients")
 require("pg")
 
 DB = PG.connect({:dbname => "hair_salon"})
+
+get("/") do
+  @stylists = Stylist.all()
+  erb(:index)
+end
+
+post("/clients") do
+  client_name = params.fetch("client_name")
+  client_id = params.fetch("client_id").to_i()
+  client = Client.new({:client_name => client_name, :client_id => client_id})
+  client.save()
+  @stylist = Stylist.find(client_id)
+  erb(:clients)
+end
+
+post("/stylists") do
+  stylist_name = params.fetch("stylist_name")
+  stylist = Stylist.new({:stylist_name => stylist_name, :id => nil})
+  stylist.save()
+  @stylists = Stylist.all()
+  erb(:index)
+end
+
+get("/stylists/:id") do
+  @stylist = Stylist.find(params.fetch("id").to_i())
+  erb(:clients)
+end
