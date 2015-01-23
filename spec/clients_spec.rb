e3c416877f00f0a0c36309e06a98103f9e29d435
Tyler Whitsett <whitsett.tyler@gmail.com>
2015-01-23 10:28:04 -0800

    add client name spec and equality override.

--- spec/clients_spec.rb
+++ spec/clients_spec.rb
@@ -7,11 +7,26 @@ describe(Client) do
     end
   end
 
+  describe("#client_name") do
+    it("returns the name of the client") do
+      test_client = Client.new({:client_name => "Becky", :client_id => nil})
+      expect(test_client.client_name()).to(eq("Becky"))
+    end
+  end
+
   describe("#save") do
     it("saves a client name to the clients array") do
-      test_client = Client.new({:client_name => "Katherine", :id => 1})
+      test_client = Client.new({:client_name => "Becky", :id => nil })
       test_client.save()
       expect(Client.all()).to(eq([test_client]))
+    end
+  end
+
+  describe("#==") do
+    it("is the same client if it has the same client name and id") do
+      test_client = Client.new({:client_name => "Becky", :id => 1 })
+      test_client2= Client.new({:client_name => "Becky", :id => 1 })
+      expect(test_client).to(eq(test_client2))
     end
   end
 
--- spec/stylists_spec.rb
+++ spec/stylists_spec.rb
@@ -10,23 +10,23 @@ describe(Stylist) do
 
   describe("#stylist_name") do
     it("returns the name of the stylist") do
-      test_stylist = Stylist.new({:stylist_name => "Becky", :stylist_id => nil})
-      expect(test_stylist.stylist_name()).to(eq("Becky"))
+      test_stylist = Stylist.new({:stylist_name => "Katherine", :stylist_id => nil})
+      expect(test_stylist.stylist_name()).to(eq("Katherine"))
     end
   end
 
   describe("#save") do
     it("saves a stylist name to the stylist class array") do
-      test_stylist = Stylist.new({:stylist_name => "Becky", :stylist_id => nil})
+      test_stylist = Stylist.new({:stylist_name => "Katherine", :stylist_id => nil})
       test_stylist.save()
       expect(Stylist.all()).to(eq([test_stylist]))
     end
   end
 
   describe("#==") do
-    it("is the same stylist if it has the same stylist name") do
-      test_stylist = Stylist.new({:stylist_name => "Becky", :id => 1 })
-      test_stylist2= Stylist.new({:stylist_name => "Becky", :id => 1 })
+    it("is the same stylist if it has the same stylist name and id") do
+      test_stylist = Stylist.new({:stylist_name => "Katherine", :id => 1 })
+      test_stylist2= Stylist.new({:stylist_name => "Katherine", :id => 1 })
       expect(test_stylist).to(eq(test_stylist2))
     end
   end
